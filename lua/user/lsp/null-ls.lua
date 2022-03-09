593ee40a5bd079e5cf0b033df5a1f91e4763f5de
Aditya Shedge <aditya.shedge@deliveryhero.com>
2022-03-09 21:56:17 +0100

    setup vim-go plugin with formatting and diagnotics for Golang with null-ls
    - formatting: go-fmt and go-imports
    - diagnotics: golangci_lint

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -14,6 +14,9 @@ null_ls.setup({
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-    -- diagnostics.flake8
+		formatting.gofmt,
+		formatting.goimports,
+		-- diagnostics.flake8
+		diagnostics.golangci_lint,
 	},
 })
